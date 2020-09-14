f70a144c08e0b1bb8401541bb705220e4af858ed
victor-alegre <victoralegre@uni.pe>
2020-09-14 02:08:45 -0500

    comentarios

--- public/script.js
+++ public/script.js
@@ -2,53 +2,59 @@ const socket = io('/')
 const videoGrid = document.getElementById('video-grid')
 /*const myPeer = new Peer(undefined, {
     host: '/',
-    port: '3001'
+    path: '/myapp',
+    port: '3000'
 })*/
 const myPeer = new Peer()
 const myVideo = document.createElement('video')
 myVideo.muted = true
 const peers = {}
 
+//Se abre la camara y microfono
 navigator.mediaDevices.getUserMedia({
     video: true,
     audio: true
 }).then(stream => {
-    addVideoStream(myVideo, stream)
+    addVideoStream(myVideo, stream) //Se muestra video del usuario actual
 
-    myPeer.on('call', call => {
-        call.answer(stream)
-        const video = document.createElement('video')
-        call.on('stream', userVideoStream => {
-            addVideoStream(video, userVideoStream)
+    myPeer.on('call', call => { //Configura evento call
+        call.answer(stream) //Responde con stream de video
+        const video = document.createElement('video') //nueva vista de video
+        call.on('stream', userVideoStream => { //Cuando se emite video
+            addVideoStream(video, userVideoStream) //Muestra en la casilla del usuario actual
         })
     })
 
-    socket.on('user-connected', userId => {
-        connectToNewUser(userId, stream)
+    socket.on('user-connected', userId => { //Cuando un usuario nuevo entra
+        connectToNewUser(userId, stream) //Conecta usuario actual con el nuevo
     })
 })
 
-socket.on('user-disconnected', userId => {
-    if (peers[userId]) peers[userId].close()
+socket.on('user-disconnected', userId => {  //Cuando un usuario se desconecto
+    if (peers[userId]) peers[userId].close()    //Cierra recepcion de su video
 })
 
-myPeer.on('open', id =>{
-    socket.emit('join-room', ROOM_ID, id)
+myPeer.on('open', id =>{    //Cuando se conecta a PeerServer
+    socket.emit('join-room', ROOM_ID, id)   //Se avisa al server que se une a la sala
 })
 
+//Funcion que conecta el usuario actual con otro
 function connectToNewUser(userId, stream){
-    const call = myPeer.call(userId, stream)
-    const video = document.createElement('video')
+    const call = myPeer.call(userId, stream) //Conecta para stream de video
+    const video = document.createElement('video') //una nueva casilla para ese usuario
     call.on('stream', userVideoStream => {
         addVideoStream(video, userVideoStream)
     })
+
+    //Cuando un usuario se desconecta
     call.on('close', () => {
-        video.remove()
+        video.remove() //Se remueve el video
     })
 
-    peers[userId] = call
+    peers[userId] = call  //Se agrega a la lista de usuarios
 }
 
+//Funcion para mostrar video en el navegador
 function addVideoStream(video, stream) {
     video.srcObject = stream
     video.addEventListener('loadedmetadata', () => {
--- server.js
+++ server.js
@@ -1,30 +1,34 @@
 const express = require('express')
 const app = express()
 const server = require('http').Server(app)
-const io = require('socket.io')(server)
-const { v4: uuidV4 } = require('uuid')
+const io = require('socket.io')(server) //se crea server socket
+const { v4: uuidV4 } = require('uuid')  //uuid que se usara para las salas
 
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs')   //modulo para renderizar plantillas
 app.use(express.static('public'))
 
-app.get('/',(req,res)=>{
-    res.redirect(`/${uuidV4()}`)
+app.get('/',(req,res)=>{    //Ruta raiz '/'
+    res.redirect(`/${uuidV4()}`)    //redirige a sala aleatoria
 })
 
-app.get('/:room',(req, res)=>{
-    res.render('room',{roomId: req.params.room})
+app.get('/:room',(req, res)=>{  //Ruta /'room-id'
+    res.render('room',{roomId: req.params.room})    //renderiza la plantilla room.ejs
 })
 
-io.on('connection', socket => {
-    socket.on('join-room', (roomId, userId) => {
+//Eventos de sockets
+io.on('connection', socket => { //Nueva conexion
+    socket.on('join-room', (roomId, userId) => {    //Cuando usuario entra a la sala
         //console.log(roomId, userId);
-        socket.join(roomId)
-        socket.to(roomId).broadcast.emit('user-connected',userId)
+        socket.join(roomId) //Subscribe socket a canal de la sala
 
-        socket.on('disconnect', () => {
+        //Notifica a todosque se conecta un nuevo usuario a la sala
+        socket.to(roomId).broadcast.emit('user-connected',userId)   
+        
+        socket.on('disconnect', () => { //Cuando se desconecta un usuario
+            //Se notifica a todos que salió un usuario
             socket.to(roomId).broadcast.emit('user-disconnected', userId)
         })
     })
 })
 
-server.listen(3000)
+server.listen(3000) //Abre puerto en el 3000
